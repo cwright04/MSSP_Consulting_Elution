8c1f1fcdb6838ffc052a5590805e6ff29048cc95
GuestUser <GuestUser@nat-wireless-guest-reg-153-51.bu.edu>
2021-11-12 15:54:49 -0500

    Updated plots for assumption checking

--- Assumption_Checking.R
+++ Assumption_Checking.R
@@ -2,6 +2,7 @@ source("DataProcessing.R", echo=FALSE)
 
 
 library("ggpubr")
+library("dplyr")
 
 col=rainbow(length(levels(factor(Elution_wide$Total_Sperm))))[factor(Elution_wide$Total_Sperm)]
 
@@ -10,35 +11,56 @@ elution_boxplot<- Elution_wide %>% ggboxplot(x="group", y="log_ratio", add="jitt
 
 elution_boxplot2 <- Elution_wide %>% ggplot(mapping = aes(x = group, y = log_ratio, group = group)) + 
     geom_boxplot() +
-  geom_point(position = "jitter",mapping = aes(size = Total_Sperm, color=Total_Sperm))
+  geom_point(position = "jitter",mapping = aes(size = Total_Sperm, color=Total_Sperm)) + 
+  ylab("Log Ratio of Elution 1 to Elution 2") + xlab("Group") + 
+  ggtitle("Boxplot of Log Ratios of Elution Counts by Group") +
+  theme(plot.title = element_text(size = (30)), 
+       legend.title = element_text(size =(20)), 
+       legend.text = element_text(size =(20)), 
+       axis.title = element_text(size = (20)),
+       axis.text = element_text(size = (20)))
 ##################################################CHECK FOR NORMALITY######################################################
     require(gridExtra)
 
   #Density Plot
-    g1_den <- ggdensity(Elution_wide1$log_ratio,main = "Density plot of Sperm Count Elution 1 - Group 1")
-    g2_den <- ggdensity(Elution_wide2$log_ratio,main = "Density plot of Sperm Count Elution 1 - Group 2")
-    g3_den <- ggdensity(Elution_wide3$log_ratio,main = "Density plot of Sperm Count Elution 1 - Group 3")
-    g4_den <- ggdensity(Elution_wide4$log_ratio,main = "Density plot of Sperm Count Elution 1 - Group 4")
+    Density_Plots <- ggplot()+ geom_density(Elution_wide, mapping = aes(x=log_ratio), alpha=0.25, color = "blue", fill = "blue") +
+      ylab("Density") +xlab("") + ggtitle("Density plot of Log Ratio of Elution 1 Count to Elution 2 Count") +
+      theme(plot.title = element_text(size = (30)), 
+            legend.title = element_text(size =(20)), 
+            legend.text = element_text(size =(20)), 
+            axis.title = element_text(size = (20)),
+            axis.text = element_text(size = (20))) +
+      facet_wrap(~ Elution_wide$group, scale = "free_y", ncol = 2) 
 
-    dens <- grid.arrange(g1_den, g2_den, g3_den, g4_den, ncol = 2)
 
   #QQPlot
-    g1_QQ <- ggqqplot(Elution_wide1$log_ratio,main = "QQ plot of Sperm Count Elution 1 - Group 1")
-    g2_QQ <- ggqqplot(Elution_wide2$log_ratio,main = "QQ plot of Sperm Count Elution 1 - Group 2")
-    g3_QQ <- ggqqplot(Elution_wide3$log_ratio,main = "QQ plot of Sperm Count Elution 1 - Group 3")
-    g4_QQ <- ggqqplot(Elution_wide4$log_ratio,main = "QQ plot of Sperm Count Elution 1 - Group 4")
+    QQ_Plots <- ggqqplot(Elution_wide$log_ratio,main = "QQ plot of Log Ratio of Elution 1 Count to Elution 2 Count") +
+      theme(plot.title = element_text(size = (30)), 
+            legend.title = element_text(size =(20)), 
+            legend.text = element_text(size =(20)), 
+            axis.title = element_text(size = (20)),
+            axis.text = element_text(size = (20))) +
+      facet_wrap(~ Elution_wide$group, scale = "free_y", ncol = 2) 
 
-    QQ<- grid.arrange(g1_QQ, g2_QQ, g3_QQ, g4_QQ, ncol = 2)
 
   #Shapiro Test--looking for a pvalue > .05
-    #Note: Group 1 does not appear to meet this assumption
-    shapiro.test(Elution_wide1$log_ratio)
-    shapiro.test(Elution_wide2$log_ratio)
-    shapiro.test(Elution_wide3$log_ratio)
-    shapiro.test(Elution_wide4$log_ratio)
+    Norm_Test_1 <- shapiro.test(Elution_wide1$log_ratio)
+    Norm_Test_2 <- shapiro.test(Elution_wide2$log_ratio)
+    Norm_Test_3 <- shapiro.test(Elution_wide3$log_ratio)
+    Norm_Test_4 <- shapiro.test(Elution_wide4$log_ratio)
+    
+    P_Value1 <- Norm_Test_1$p.value
+    P_Value2 <- Norm_Test_2$p.value
+    P_Value3 <- Norm_Test_3$p.value
+    P_Value4 <- Norm_Test_4$p.value
+    
+    Shap_Test_P_Value <- data.frame(cbind(P_Value1,P_Value2,P_Value3,P_Value4))
+    Shap_Test_P_Value <- Shap_Test_P_Value %>% mutate(alpha = .05)
+    names(Shap_Test_P_Value) <- c("Group1 P-Value","Group2 P-Value","Group3 P-Value","Group4 P-Value","Alpha Level")
     
     
-    grid.arrange(dens, QQ, ncol =1)
-    elution_boxplot
+Density_Plots
+QQ_Plots
+elution_boxplot
     elution_boxplot2
     
